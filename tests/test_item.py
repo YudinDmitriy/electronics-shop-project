028a690e1c36dad87b427e080cf25ca7612cb66c
Yudin Dmitriy <yudin01042002@yandex.ru>
2024-01-29 21:08:50 +0300

    add homework-1

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,15 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
+from src.item import *
+
+@pytest.fixture
+def item_smartph():
+    return Item("Смартфон", 10000, 20)
+
+def test_calculate_total_price(item_smartph):
+    assert item_smartph.calculate_total_price() == 200000
+
+def test_apply_discount(item_smartph):
+    Item.pay_rate = 0.5
+    assert item_smartph.apply_discount() == 5000
