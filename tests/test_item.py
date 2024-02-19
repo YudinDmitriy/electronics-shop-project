623656cdd176b923d4841f9a83fc46c194dfdbd3
Yudin Dmitriy <yudin01042002@yandex.ru>
2024-02-19 16:06:57 +0300

    3 homework

--- tests/test_item.py
+++ tests/test_item.py
@@ -27,3 +27,11 @@ def test_string_to_number(item_smartph):
 def test_instantiate_from_csv():
     Item.instantiate_from_csv('../src/items.csv')
     assert len(Item.all) == 5
+
+def test_repr():
+    item1 = Item("Смартфон", 10000, 20)
+    assert repr(item1) == "Item('Смартфон', 10000, 20)"
+
+def test_str():
+    item1 = Item("Смартфон", 10000, 20)
+    assert str(item1) == 'Смартфон'
